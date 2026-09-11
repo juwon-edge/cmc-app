adbe3c4655770ca5be0bc7055c08071853399654
juwon-edge <juwonchina@gmail.com>
2026-09-11 18:21:45 +0100

    moved react-native-botsort to dedicated repo

--- react-native-botsort/android/src/main/cpp/cpp-adapter.cpp
+++
@@ -1,9 +0,0 @@
-#include <jni.h>
-#include <fbjni/fbjni.h>
-#include "NitroBotsortOnLoad.hpp"
-
-JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void*) {
-  return facebook::jni::initialize(vm, []() {
-    margelo::nitro::botsort::registerAllNatives();
-  });
-}
--- react-native-botsort/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { NitroModules } from 'react-native-nitro-modules'
-import type { BoTSortTracker } from './specs/BoTSortTracker.nitro'
-
-export const BoTSort =
-  NitroModules.createHybridObject<BoTSortTracker>('BoTSortTracker')
-
-export * from './specs/BoTSortTracker.nitro'
